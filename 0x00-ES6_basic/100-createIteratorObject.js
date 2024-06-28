05f70c5115c20fc706b506446c30723fd6927fdd
DianaWangui <dianawanguikt048@gmail.com>
2024-06-28 21:48:38 +0300

    advanced

---
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -0,0 +1,7 @@
+export default function createIteratorObject(report) {
+    const employees = [];
+    for (const emp of Object.keys(report.allEmployees)) {
+        employees.push([...report.allEmployees[emp]]);
+    }
+    return employees;
+}
