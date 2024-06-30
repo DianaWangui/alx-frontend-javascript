be251a5fc98c2226900aa252896e33df8e3946b5
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 16:09:46 +0300

    task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { createUser, uploadPhoto } from "./utils";
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    const [photo, user] = values;
+    console.log(photo.body, user.firstName, user.lastName)
+  })
+  .catch(() => {
+    console.log('Signup system offline');
+  });
+}
