60fc386c30f5afdd9400700ba578fbccc4e2e924
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 16:45:09 +0300

    nnew weer

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,14 @@
 import { createUser, uploadPhoto } from "./utils";
+
 export default function handleProfileSignup() {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => { 
     Promise.all([uploadPhoto(), createUser()])
       .then((values) => {
         const [photo, user] = values;
         console.log(photo.body, user.firstName, user.lastName);
+        resolve();
       })
-      .catch(() => console.log('Signup system offline'));
+      .catch(() => console.log("Signup system offline"));
+      reject();
   });
 }
