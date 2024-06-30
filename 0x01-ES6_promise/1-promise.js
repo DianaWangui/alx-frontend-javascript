3270a45b0b6869d53a44e651a97b409f45a1bd29
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 15:16:14 +0300

    indent

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-export function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({ status: 200, body: 'Success' });
-        } else {
-            reject(new Error('The fake API is not working currently'));
-        }
-    });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
