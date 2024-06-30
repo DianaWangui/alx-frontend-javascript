3b68e8aeb0986e73259e3789863b97cf0f57c994
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 14:52:46 +0300

    error

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,9 +1,11 @@
 export default function getResponseFromAPI() {
-    return new Promise(function(resolve, reject) {
-        if (true) {
-            resolve('success');
+    return new Promise((resolve, reject) => {
+        const myresponse = true;
+        if (myresponse) {
+            resolve();
         } else {
-            reject('error');
+            reject();
         }
+        
     });
 }
