79275fc49fcda0a4df576c9e2a1ff8fa64e771cd
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 17:08:10 +0300

    promise error

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,15 +1,11 @@
 export default function handleResponseFromAPI(promise) {
-  return new Promise((resolve, reject) => {
-    try {
-      if (promise) {
-        resolve({ status: 200, body: 'success' });
-      } else {
-        reject(new Error());
-      }
-    } catch (error) {
-      reject(new Error());
-    } finally {
+  return promise
+    .then(() => {
       console.log('Got a response from the API');
-    }
-  });
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
