9f3b379f7a297d3d6abbe92ff38bfd01e2d2b286
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 15:05:22 +0300

    indent

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,10 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-        const myresponse = true;
-        if (myresponse) {
-            resolve('s');
-        } else {
-            reject(new Error('error'));
-        }
-        
-    });
+  return new Promise((resolve, reject) => {
+    const myresponse = true;
+    if (myresponse) {
+      resolve('s');
+    } else {
+      reject(new Error('error'));
+    }
+  });
 }
