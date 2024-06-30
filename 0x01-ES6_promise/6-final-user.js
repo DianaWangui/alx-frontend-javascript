10f9839eb4fbd04c56b29629d89b6af65dce8df3
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 18:11:40 +0300

    task 6

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  const promises = [
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename)
+  ];
+  return Promise.allSettled(promises)
+    .then((results) => {
+      return results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason.message
+      }));
+    })
+    .catch((error) => {
+      console.log('Error: ', error);
+    });
+}
