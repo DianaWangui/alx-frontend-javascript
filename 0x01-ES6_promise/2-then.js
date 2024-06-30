4eda5d354bfe32ac3ed07c581ae9b0a0cf9b4228
DianaWangui <dianawanguikt048@gmail.com>
2024-06-30 15:23:31 +0300

    task 2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,10 @@
+export default function handleResponseFromAPI(promise) {
+  return new Promise((resolve, reject) => {
+    if (promise) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error());
+    }
+    console.log('Got a response from the API');
+  });
+}
