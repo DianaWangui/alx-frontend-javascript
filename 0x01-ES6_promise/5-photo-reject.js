320a54bd7d3f81df7aae37e517e7d08e3827c913
DianaWangui <dianawanguikt048@gmail.com>
2024-07-01 08:25:32 +0300

    import

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
   return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
+    reject(Error(`${filename} cannot be processed`));
   });
 }
