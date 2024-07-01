b697592e97da1375082d162dc08afcb202709190
DianaWangui <dianawanguikt048@gmail.com>
2024-07-01 08:42:45 +0300

    error

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,14 +6,12 @@ export default function handleProfileSignup(firstName, lastName, filename) {
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
   ];
-  return Promise.allSettled(promises).then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value
+  return Promise.allSettled(promises).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value
       : result.reason.message,
-    }));
-  })
-  .catch((error) => {
-    console.log('Error: ', error);
-  });
+  })))
+    .catch((error) => {
+      console.log('Error: ', error);
+    });
 }
