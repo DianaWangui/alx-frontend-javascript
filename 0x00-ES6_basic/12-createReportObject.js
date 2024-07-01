5328969ccbc258be675c0330d71e2a8f9d250da8
DianaWangui <dianawanguikt048@gmail.com>
2024-07-01 10:49:26 +0300

    more eroor

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,6 +1,6 @@
 export default function createReportObject(employeesList) {
-    return {
-        getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
-        allEmployees: {...employeesList }
-    };
+  return {
+    getNumberOfDepartments: (employeesList) => Object.keys(employeesList).length,
+    allEmployees: { ...employeesList },
+  };
 }
