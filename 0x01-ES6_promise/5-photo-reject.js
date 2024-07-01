c7dbf29645225d4f4fd30a440f92ccff4031a90d
DianaWangui <dianawanguikt048@gmail.com>
2024-07-01 08:33:06 +0300

    error

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(Error(`${filename} cannot be processed`));
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${filename} cannot be processed`));
   });
 }
